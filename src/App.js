14463f8ec2b99ec4a51eaad502ee002807dc0361
PROGVIDE <prgovide@gmail.com>
2021-10-10 16:58:36 +0500

    Create UI, Context, useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 //import logo from './logo.svg';
 import "./App.css";
+import { AccountSummary } from "./components/AccountSummary";
+import { AddTransaction } from "./components/AddTransaction";
+import { Balance } from "./components/Balance";
+import { Header } from "./components/Header";
+import { TransactionHistory } from "./components/TransactionHistory";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
-  return <div>Hello World.</div>;
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <AccountSummary />
+        <TransactionHistory />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
 }
 
 export default App;
